8fff50ad2aef8a067a46b234183791c32be30aa5
xiew <xiew@wanthings.com>
2020-10-30 17:00:42 +0800

    binance

---
+++ exchange/binance/bianace-trade.go
@@ -0,0 +1,109 @@
+/*
+获取市场逐笔交易
+api文档: https://binance-docs.github.io/apidocs/spot/cn/#2b149598d9
+*/
+package binance
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+/*
+"e": "trade",     // 事件类型
+"E": 123456789,   // 事件时间
+"s": "BNBBTC",    // 交易对
+"t": 12345,       // 交易ID
+"p": "0.001",     // 成交价格
+"q": "100",       // 成交笔数
+"b": 88,          // 买方的订单ID
+"a": 50,          // 卖方的订单ID
+"T": 123456785,   // 成交时间
+"m": true,        // 买方是否是做市方。如true，则此次成交是一个主动卖出单，否则是一个主动买入单。
+"M": true         // 请忽略该字段
+*/
+type BinanceTrade struct {
+	EL string `json:"e"`
+	E  int    `json:"E"`
+	SL string `json:"s"`
+	TL string `json:"t"`
+	PL string `json:"p"`
+	QL string `json:"q"`
+	BL int    `json:"b"`
+	AL int    `json:"a"`
+	T  int    `json:"T"`
+	ML bool   `json:"m"`
+	M  bool   `json:"M"`
+}
+
+func (h *BinanceWsConn) StartMarketDetail() {
+	// 创建ws
+	for {
+		if h.createConnection() {
+			break
+		}
+	}
+	defer h.wsConn.Close()
+
+	// 订阅
+	for {
+		if h.subscribeMarketDetail() {
+			break
+		}
+	}
+
+	// ping
+	go h.ping()
+
+	go h.readMarketDetail()
+
+	select {}
+}
+
+/*
+订阅频道
+*/
+func (h *BinanceWsConn) subscribeMarketDetail() bool {
+	symbol := h.symbol.ToLowerWithSep("")
+	streamName := fmt.Sprintf("%s@trade", symbol)
+	message, _ := json.Marshal(map[string]interface{}{
+		"method": "SUBSCRIBE",
+		"params": []string{streamName},
+		"id":     1,
+	})
+	if !h.writeMessage(message) {
+		return false
+	}
+
+	return true
+}
+
+/*
+获取市场逐笔成交
+*/
+func (h *BinanceWsConn) readMarketDetail() {
+	defer func() {
+		go h.StartMarketDetail()
+	}()
+
+	for {
+		data, err := h.readMessage()
+		if err != nil {
+			h.ErrChan <- err
+		}
+
+		dataStr := string(data)
+		if strings.Contains(dataStr, "result") { // 订阅成功消息
+			continue
+		} else {
+			rt := BinanceTrade{}
+			err := json.Unmarshal(data, &rt)
+			if err != nil {
+				h.ErrChan <- err
+			} else {
+				h.OutChan <- rt
+			}
+		}
+	}
+}
--- exchange/binance/binance-ws.go
+++ exchange/binance/binance-ws.go
@@ -1,29 +1,23 @@
-package huobipro
+package binance
 
 import (
-	"bytes"
-	"compress/gzip"
 	"crypto/tls"
-	"encoding/binary"
-	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/xiiiew/eqs/eqsModels"
 	"github.com/xiiiew/eqs/eqsWebsocket"
-	"io/ioutil"
 	"net/url"
 	"time"
 )
 
 const DefaultScheme = "wss"
-const DefaultHost = "api.huobi.pro"
+const DefaultHost = "stream.binance.com:9443"
 const DefaultPath = "/ws"
 
-type HuobiproWsConn struct {
+type BinanceWsConn struct {
 	scheme  string
 	host    string
 	path    string
 	symbol  eqsModels.EqsSymbol
-	id      string
 	OutChan chan interface{}
 	ErrChan chan error
 	wsConn  *eqsWebsocket.Connection
@@ -32,15 +26,14 @@ type HuobiproWsConn struct {
 /*
 使用默认地址, 大陆网络慎用
 */
-func NewDefauldHuobiproWsConn(symbol eqsModels.EqsSymbol, id string) *HuobiproWsConn {
+func NewDefauldBinanceWsConn(symbol eqsModels.EqsSymbol, id string) *BinanceWsConn {
 	outChan := make(chan interface{}, 100)
 	errChan := make(chan error, 100)
-	return &HuobiproWsConn{
+	return &BinanceWsConn{
 		scheme:  DefaultScheme,
 		host:    DefaultHost,
 		path:    DefaultPath,
 		symbol:  symbol,
-		id:      id,
 		OutChan: outChan,
 		ErrChan: errChan,
 	}
@@ -50,15 +43,14 @@ func NewDefauldHuobiproWsConn(symbol eqsModels.EqsSymbol, id string) *HuobiproWs
 替换默认地址
 api文档: https://huobiapi.github.io/docs/spot/v1/cn/#7c47ef3411
 */
-func NewHuobiproWsConnWithHost(scheme string, host string, symbol eqsModels.EqsSymbol, id string) *HuobiproWsConn {
+func NewBinanceWsConnWithHost(scheme string, host string, symbol eqsModels.EqsSymbol) *BinanceWsConn {
 	outChan := make(chan interface{}, 100)
 	errChan := make(chan error, 100)
-	return &HuobiproWsConn{
+	return &BinanceWsConn{
 		scheme:  scheme,
 		host:    host,
 		path:    DefaultPath,
 		symbol:  symbol,
-		id:      id,
 		OutChan: outChan,
 		ErrChan: errChan,
 	}
@@ -67,7 +59,7 @@ func NewHuobiproWsConnWithHost(scheme string, host string, symbol eqsModels.EqsS
 /*
 创建websocket连接
 */
-func (h *HuobiproWsConn) createConnection() bool {
+func (h *BinanceWsConn) createConnection() bool {
 	u := url.URL{
 		Scheme: h.scheme,
 		Host:   h.host,
@@ -96,7 +88,7 @@ func (h *HuobiproWsConn) createConnection() bool {
 /*
 发送数据
 */
-func (h *HuobiproWsConn) writeMessage(bytes []byte) bool {
+func (h *BinanceWsConn) writeMessage(bytes []byte) bool {
 	err := h.wsConn.WriteMessage(bytes)
 	if err != nil {
 		h.ErrChan <- err
@@ -108,45 +100,21 @@ func (h *HuobiproWsConn) writeMessage(bytes []byte) bool {
 /*
 读取数据
 */
-func (h *HuobiproWsConn) readMessage() ([]byte, error) {
+func (h *BinanceWsConn) readMessage() ([]byte, error) {
 	data, err := h.wsConn.ReadMessage()
 	if err != nil {
 		return nil, err
 	}
 
-	return h.unzip(data)
-}
-
-/*
-解压
-*/
-func (h *HuobiproWsConn) unzip(bytesData []byte) ([]byte, error) {
-	b := new(bytes.Buffer)
-	err := binary.Write(b, binary.LittleEndian, bytesData)
-	if err != nil {
-		return nil, err
-	}
-
-	r, err := gzip.NewReader(b)
-	if err != nil {
-		return nil, err
-	}
-	defer r.Close()
-
-	unzipData, err := ioutil.ReadAll(r)
-	if err != nil {
-		return nil, err
-	}
-
-	return unzipData, nil
+	return data, err
 }
 
 /*
 ping
 */
-func (h *HuobiproWsConn) ping() {
+func (h *BinanceWsConn) ping() {
 	for {
-		msg := []byte(fmt.Sprintf("{\"ping\" : %d}", time.Now().Unix()))
+		msg := []byte("{\"method\": \"GET_PROPERTY\", \"params\":[\"combined\"],\"id\":2}")
 		if ! h.writeMessage(msg) {
 			return
 		}
